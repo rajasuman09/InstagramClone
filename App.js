8e8156d8d9f3090ebc8f9f4b2da8a18094f66c18
rajasuman09 <rajasuman09@gmail.com>
2022-06-04 17:36:40 +0530

    Updated Home Screen with footer

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./app/components/Header";
 import Screen from "./app/components/Screen";
 import Stories from "./app/components/Stories";
+import HomeScreen from "./app/screens/HomeScreen";
 
 export default function App() {
-  return (
-    <Screen customStyles={styles.container}>
-      <StatusBar style="light" />
-      <Header />
-      <Stories />
-    </Screen>
-  );
+  return <HomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000",
-    paddingHorizontal: 10,
-  },
-});
